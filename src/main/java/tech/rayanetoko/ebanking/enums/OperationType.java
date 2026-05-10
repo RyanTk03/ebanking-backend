5cf64215ee951f887baa8fa94640ebaa24865507
ryntk <rayanetoko@proton.me>
2026-05-10 16:05:27 +0100

    setup entities

---
+++ src/main/java/tech/rayanetoko/ebanking/enums/OperationType.java
@@ -0,0 +1,5 @@
+package tech.rayanetoko.ebanking.enums;
+
+public enum OperationType {
+    DEBIT, CREDIT
+}
---
+++ src/test/java/tech/rayanetoko/ebanking/EbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.rayanetoko.ebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
